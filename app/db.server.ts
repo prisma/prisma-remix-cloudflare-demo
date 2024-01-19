3cfa509a06879896b99590b485ee0f923bd1fb58
jkomyno <skiabo97@gmail.com>
2024-01-19 05:06:29 +0100

    feat: set up Cloudflare Pages with Pg, resulting in failures

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client/edge";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool } from "@neondatabase/serverless";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 export function getDB(url: string) {
   const pool = new Pool({ connectionString: url });
-  const adapter = new PrismaNeon(pool);
+  const adapter = new PrismaPg(pool);
   return new PrismaClient({ adapter });
 }
